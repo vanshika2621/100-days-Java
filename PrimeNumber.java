6bf6d9c674a3e5bd77bfcc5668f235da21529779
vanshika2621 <vanshikagiri2020@gmail.com>
2026-07-05 23:16:14 +0530

    Create PrimeNumber.java

---
+++ PrimeNumber.java
@@ -0,0 +1,15 @@
+// Create a method to check if a number is prime.
+
+public class PrimeNumber {
+    //Method to check if a number is prime
+    public static int Prime(int num){
+        if(num<=2|| num%2 == 0){
+            System.out.println("Not a Prime number ")
+        }
+        for(int i=3; i<=Math.sqrt(num); i+=2){
+            if(num%i==0){
+                System.out.println("Not a Prime number ")
+            }
+        }
+    }
+}
