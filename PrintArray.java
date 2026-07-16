2a58e67038079650f21a311ae6b5752aac059d51
vanshika2621 <vanshikagiri2020@gmail.com>
2026-07-16 12:54:00 +0530

    Create PrintArray.java

---
+++ PrintArray.java
@@ -0,0 +1,14 @@
+// Print all elements of an array
+
+import java.util.Arrays;
+
+public class PrintArray{
+    public static void main(String[] args){
+        
+        int[] a={1,2,3,4,5,6,7,8} ;
+        
+        System.out.println(Arrays.toString(a)); 
+        
+        
+    }
+}
