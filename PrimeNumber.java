a3f0923da51a07ed04e86ece7d3447ad9639a635
vanshika2621 <vanshikagiri2020@gmail.com>
2026-07-20 22:21:42 +0530

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -4,12 +4,14 @@ public class PrimeNumber {
     //Method to check if a number is prime
     public static int Prime(int num){
         if(num<=2|| num%2 == 0){
-            System.out.println("Not a Prime number ")
+            System.out.println("Not a Prime number ");
         }
         for(int i=3; i<=Math.sqrt(num); i+=2){
             if(num%i==0){
-                System.out.println("Not a Prime number ")
+                System.out.println("Not a Prime number ");
             }
         }
+        System.out.println("Prime number ");
+        return num;
     }
 }
